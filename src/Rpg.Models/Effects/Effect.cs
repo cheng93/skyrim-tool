7fd0e50a2b603ae2165b7602481cf865f007aaf3
cheng93 <derek.c@hotmail.co.uk>
2018-03-13 20:20:39 +0000

    IHasName

--- src/Rpg.Models/Effects/Effect.cs
+++ src/Rpg.Models/Effects/Effect.cs
@@ -1,8 +1,10 @@
+using Rpg.Models.Common;
+
 namespace Rpg.Models.Effects
 {
     public interface IEffect<T> {}
 
-    public abstract class Effect<T> : IEffect<T>
+    public abstract class Effect<T> : IEffect<T>, IHasName
         where T : Effect<T>
     {
         protected Effect(string name)
