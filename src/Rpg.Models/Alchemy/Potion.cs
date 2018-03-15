9d714ad5dd0bac00298507fb1db7b92071c21770
cheng93 <derek.c@hotmail.co.uk>
2018-03-15 08:03:51 +0000

    IPotion

---
+++ src/Rpg.Models/Alchemy/Potion.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using Rpg.Models.Common;
+
+namespace Rpg.Models.Alchemy
+{
+    public interface IPotion : IHasName, IHasDescription
+    {
+        IEnumerable<IAlchemyEffect> Effects { get; }
+    }
+
+    public class Potion : IPotion
+    {
+        public Potion(
+            IEnumerable<IAlchemyEffect> effects,
+            string name)
+        {
+            Effects = effects;
+            Name = name;
+            Description = string.Join(", ", effects.Select(x => x.Description));
+                
+        }
+
+        public IEnumerable<IAlchemyEffect> Effects { get; }
+
+        public string Name { get; }
+
+        public string Description { get; }
+    }
+}
