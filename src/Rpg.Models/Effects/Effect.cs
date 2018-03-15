a2359175cd561b92cffb2a376cc1952be0bfe002
cheng93 <derek.c@hotmail.co.uk>
2018-03-15 20:23:23 +0000

    contraint on interface

--- src/Rpg.Models/Effects/Effect.cs
+++ src/Rpg.Models/Effects/Effect.cs
@@ -2,10 +2,13 @@ using Rpg.Models.Common;
 
 namespace Rpg.Models.Effects
 {
-    public interface IEffect<T> : IHasName {}
+    public interface IEffect<T> : IHasName
+        where T : IEffect<T>
+    {
+    }
 
     public abstract class Effect<T> : IEffect<T>
-        where T : Effect<T>
+        where T : IEffect<T>
     {
         protected Effect(string name)
         {
