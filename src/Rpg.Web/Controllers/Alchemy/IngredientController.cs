1a626b51a8f6d5bae5b0c1f4e78e317b6cae9d6b
cheng93 <derek.c@hotmail.co.uk>
2018-03-21 22:08:52 +0000

    add api controllers

---
+++ src/Rpg.Web/Controllers/Alchemy/IngredientController.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Rpg.Commands.Alchemy;
+
+namespace Rpg.Web.Controllers.Alchemy
+{
+    [Route("api/alchemy/ingredients")]
+    public class IngredientController : BaseController
+    {
+        public IngredientController(IMediator mediator) : base(mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IngredientList()
+        {
+            var command = new IngredientList.Command();
+            var response = await Mediator.Send(command);
+
+            return Json(response);
+        }
+    }
+}
